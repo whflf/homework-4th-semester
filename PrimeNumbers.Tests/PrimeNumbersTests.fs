fb72f3ffef3c45b87bea127faa6114f389af0c45
Елена Макарова <fluffy.jesus@yandex.ru>
2025-03-18 18:28:02 +0300

    add task files

---
+++ PrimeNumbers.Tests/PrimeNumbersTests.fs
@@ -0,0 +1,47 @@
+﻿module PrimeNumbers.Tests
+
+open NUnit.Framework
+open FsUnit
+open PrimeNumbers
+
+let firstNPrimes n =
+    initInfinitePrimeNumbers () |> Seq.take n |> Seq.toList
+
+[<Test>]
+let ``First 5 primes should be [2; 3; 5; 7; 11]`` () =
+    firstNPrimes 5 |> should equal [2; 3; 5; 7; 11]
+
+[<Test>]
+let ``All elements should be prime`` () =
+    firstNPrimes 100 |> List.forall isPrime |> should be True
+
+[<Test>]
+let ``Sequence should be in ascending order`` () =
+    firstNPrimes 100 |> should be ascending
+
+[<Test>]
+let ``Large number of elements should be initialized correctly`` () =
+    Seq.isEmpty (firstNPrimes 50000) |> should be False
+
+[<Test>]
+let ``Elements should be unique`` () =
+    let primes = firstNPrimes 5000
+    let listLength = List.length (Seq.toList (primes))
+    let setCount = Set.count (Set.ofSeq (primes))
+    listLength |> should equal setCount
+
+[<Test>]
+let ``Elements should be prime`` () =
+    let primes = Seq.toList (firstNPrimes 500)
+    let filteredPrimes = List.filter (fun x -> isPrime x) primes
+    let lengthFiltered = List.length filteredPrimes
+    List.length primes |> should equal lengthFiltered
+
+[<Test>]
+let ``First prime should be 2`` () =
+    Seq.item 0 (initInfinitePrimeNumbers ()) |> should equal 2
+
+[<Test>]
+let ``1000th prime should be 7919`` () =
+    Seq.item 999 (initInfinitePrimeNumbers ()) |> should equal 7919
+
---
+++ PrimeNumbers/Program.fs
@@ -0,0 +1,30 @@
+﻿module PrimeNumbers
+
+let isPrime n =
+    if n < 2 then false
+    else
+        let sqrtN = int (sqrt (float n))
+        [2..sqrtN] |> List.forall (fun x -> n % x <> 0)
+
+let initInfinitePrimeNumbers () =
+    let rec nextPrime n =
+        if isPrime n then n
+        else nextPrime (n + 1)
+
+    let rec generatePrimes n =
+        seq {
+            let prime = nextPrime n
+            yield prime
+            yield! generatePrimes (prime + 1)
+        }
+
+    generatePrimes 2
+
+let firstNPrimes n =
+    let primeNumbersSequence = initInfinitePrimeNumbers ()
+    primeNumbersSequence |> Seq.take n |> Seq.toList
+
+// Пример использования
+let result = firstNPrimes 10
+printfn "%A" result
+        
