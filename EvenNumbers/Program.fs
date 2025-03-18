f443484c36923f03f61c858a82cf5317bbabe290
Елена Макарова <fluffy.jesus@yandex.ru>
2025-03-18 18:30:31 +0300

    add task files

---
+++ EvenNumbers/Program.fs
@@ -0,0 +1,13 @@
+﻿module EvenNumbers
+
+let countEvenNumbersMap list = 
+    List.map (fun x -> if x % 2 = 0 then 1 else 0) list |> List.fold (+) 0
+
+let countEvenNumbersFilter list =
+    List.filter (fun x -> x % 2 = 0) list |> List.length
+
+let countEvenNumbersFold list = 
+    List.fold (fun acc number -> 
+                (if (number % 2 = 0) then acc + 1 else acc)) 0 list
+
+printfn "%A" <| countEvenNumbersFold [ 1; 2; 3; 4; 6; 2; 8 ]
