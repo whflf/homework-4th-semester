9ceadf3ec9378c7826c204a3803c9ffa4ef4c200
Елена Макарова <fluffy.jesus@yandex.ru>
2025-03-21 00:37:16 +0300

    fix tests

--- PointFree.Tests/PointFreeTests.fs
+++ PointFree.Tests/PointFreeTests.fs
@@ -1,14 +1,18 @@
 ﻿module PointFree.Tests
 
 open NUnit.Framework
-
-open FsCheck
+open FsUnit
+open FsCheck.NUnit
 
 open PointFree
 
-[<Test>]
+[<Property>]
 let testFunc (x: int) (l: int list) =
-    func'3 x l = func x l
+    let func'3 : int -> int list -> int list = (*) >> List.map
+    func'3 (x) (l) = func x l
 
-Check.Quick testFunc
-
+[<Test>]
+let ``func'3 is equivalent to func`` () =
+    let func'3 : int -> int list -> int list = (*) >> List.map
+    func 5 [1; 2; 3] |> should equal (func'3 (5) ([1; 2; 3]))
+    
--- PointFree/Program.fs
+++ PointFree/Program.fs
@@ -5,5 +5,5 @@ let func x l = List.map (fun y -> y * x) l
 let func'1 x l = List.map ((*) x) l
 
 let func'2 x : int list -> int list = List.map ((*) x)
- 
-let func'3 : int -> int list -> int list = List.map << (*)
+
+let func'3 : int -> int list -> int list = (*) >> List.map
