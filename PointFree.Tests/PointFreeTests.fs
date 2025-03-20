bed3338fc49b737c4333cb7f9d480c895bade16d
Елена Макарова <fluffy.jesus@yandex.ru>
2025-03-20 23:58:31 +0300

    add task files

---
+++ PointFree.Tests/PointFreeTests.fs
@@ -0,0 +1,14 @@
+﻿module PointFree.Tests
+
+open NUnit.Framework
+
+open FsCheck
+
+open PointFree
+
+[<Test>]
+let testFunc (x: int) (l: int list) =
+    func'3 x l = func x l
+
+Check.Quick testFunc
+
---
+++ PointFree/Program.fs
@@ -0,0 +1,9 @@
+﻿module PointFree
+
+let func x l = List.map (fun y -> y * x) l
+
+let func'1 x l = List.map ((*) x) l
+
+let func'2 x : int list -> int list = List.map ((*) x)
+ 
+let func'3 : int -> int list -> int list = List.map << (*)
