1cd0ea1120cd0c215540a8eefda21afadb24ec4d
Елена Макарова <fluffy.jesus@yandex.ru>
2025-04-10 17:13:52 +0300

    add task files

---
+++ TestTasks/InfiniteSequence.fs
@@ -0,0 +1,11 @@
+﻿module InfiniteSequence
+
+let alternatingSigns = Seq.initInfinite(fun i -> pown -1 i)
+
+let initInfiniteSequence () = 
+    let alternatingNumbers =
+        Seq.initInfinite (fun i -> i + 1)
+        |> Seq.zip alternatingSigns
+        |> Seq.map (fun (sign, value) -> sign * value)
+
+    alternatingNumbers
