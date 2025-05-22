014bb32c3e77cb3f8021c67ce46076a15a1faf74
Елена Макарова <fluffy.jesus@yandex.ru>
2025-05-22 23:36:52 +0300

    add task files

---
+++ LambdaParser.Tests/LambdaParserTests.fs
@@ -0,0 +1,119 @@
+﻿module LambdaParser.Tests
+
+open NUnit.Framework
+open FsUnit
+open LambdaInterpreter
+open LambdaParser
+open FParsec
+
+
+[<Test>]
+let ``S K K result should be \z.z`` () =
+    let testInput = "let S = \x y z.x z (y z)
+                    let K = \x y.x
+                    S K K"
+    let result = testInput |> run program
+
+    let reducted =
+        match result with
+        | Success(result, _, _) -> toString (betaReduction (buildAST result))
+        | Failure(errorMessage, _, _) -> failwith errorMessage
+
+    reducted |> should equal "\z.z"
+
+[<Test>]
+let ``succ (succ (plus one zero)) result should be \f.\x.f (f (f x))`` () =
+    let testInput = "let succ = \\n f x.f (n f x)
+                    let plus = \\n m f x. n f (m f x)
+                    let one = \\f x. f x
+                    let zero = \\f x.x
+                    succ (succ (plus one zero))"
+    let result = testInput |> run program
+
+    let reducted =
+        match result with
+        | Success(result, _, _) -> toString (betaReduction (buildAST result))
+        | Failure(errorMessage, _, _) -> failwith errorMessage
+
+    reducted |> should equal "\\f.\x.f (f (f x))"
+
+[<Test>]
+let ``S result should be \x.\y.\z.x z (y z)`` () =
+    let testInput = "let S = \x y z.x z (y z)
+                    S"
+    let result = testInput |> run program
+
+    let reducted =
+        match result with
+        | Success(result, _, _) -> toString (betaReduction (buildAST result))
+        | Failure(errorMessage, _, _) -> failwith errorMessage
+
+    reducted |> should equal "\x.\y.\z.x z (y z)"
+
+[<Test>]
+let ``K a b result should be a`` () =
+    let testInput = "let K = \x y.x
+                    K a b"
+    let result = testInput |> run program
+
+    let reducted =
+        match result with
+        | Success(result, _, _) -> toString (betaReduction (buildAST result))
+        | Failure(errorMessage, _, _) -> failwith errorMessage
+
+    reducted |> should equal "a"
+
+
+[<Test>]
+let ``S a b c result should be a c (b c)`` () =
+    let testInput = "let S = \x y z.x z (y z)
+                    S a b c"
+    let result = testInput |> run program
+
+    let reducted =
+        match result with
+        | Success(result, _, _) -> toString (betaReduction (buildAST result))
+        | Failure(errorMessage, _, _) -> failwith errorMessage
+
+    reducted |> should equal "a c (b c)"
+
+[<Test>]
+let ``S K K x result should be x`` () =
+    let testInput = "let S = \x y z.x z (y z)
+                    let K = \x y.x
+                    S K K x"
+    let result = testInput |> run program
+
+    let reducted =
+        match result with
+        | Success(result, _, _) -> toString (betaReduction (buildAST result))
+        | Failure(errorMessage, _, _) -> failwith errorMessage
+
+    reducted |> should equal "x"
+
+[<Test>]
+let ``F a b result should be b`` () =
+    let testInput = "let F = \x y.y
+                    F a b"
+    let result = testInput |> run program
+
+    let reducted =
+        match result with
+        | Success(result, _, _) -> toString (betaReduction (buildAST result))
+        | Failure(errorMessage, _, _) -> failwith errorMessage
+
+    reducted |> should equal "b"
+
+[<Test>]
+let ``first (pair x y) result should be x`` () =
+    let testInput = "let pair = \\a b f.f a b
+                    let first = \p.p (\x y.x)
+                    first (pair x y)"
+    let result = testInput |> run program
+
+    let reducted =
+        match result with
+        | Success(result, _, _) -> toString (betaReduction (buildAST result))
+        | Failure(errorMessage, _, _) -> failwith errorMessage
+
+    reducted |> should equal "x"
---
+++ LambdaParser/LambdaParser.fs
@@ -0,0 +1,100 @@
+﻿module LambdaParser
+
+open System.Collections.Generic
+open FParsec
+
+open LambdaInterpreter
+
+type Program =
+| Program of Dictionary<Term', Term> * Term
+and Term =
+| Atom of Term'
+| Application of Term * Term
+and Term' =
+| Abstraction of string list * Term
+| Variable of string
+| Brackets of Term
+
+let rec buildAST input =
+    let rec buildTerm (dict : Dictionary<Term', Term>) = function
+        | Atom(term') -> buildTerm' dict term'
+        | Application(term1, term2) -> 
+            LambdaTerm.Application(buildTerm dict term1, buildTerm dict term2)
+    and buildTerm' (dict : Dictionary<Term', Term>) = function
+        | Abstraction(vars, term) ->
+            let rec processVars list =
+                match list with
+                | head :: tail -> LambdaTerm.Abstraction(head, processVars tail)
+                | [] -> buildTerm dict term
+            processVars vars
+        | Brackets(term) -> buildTerm dict term
+        | Variable(var) -> 
+            let term'Var = Term'.Variable(var)
+            if dict.ContainsKey(term'Var) then buildTerm dict dict[term'Var]
+            else LambdaTerm.Variable(var)
+
+    match input with
+    | Program(dict, expr) -> buildTerm dict expr
+
+let spacesWithoutNewline : Parser<unit, unit> = 
+    skipMany (satisfy (fun c -> c = ' ' || c = '\t'))
+
+let (!) parser = parser .>> spacesWithoutNewline
+let (!!) parser = parser .>> spaces
+
+let term, termRef = createParserForwardedToRef()
+
+let term', term'Ref = createParserForwardedToRef()
+
+let var = 
+    asciiLetter .>>. many (asciiLetter <|> digit)
+    |>> fun (first, rest) -> 
+        let varName = string first + String.concat "" (List.map string rest)
+        Variable varName
+
+let varList = 
+    many1 (!var |>> function Variable name -> name | _ -> failwith "Not a variable")
+
+term'Ref := 
+    (!(pchar '\\') >>. !varList .>> !(pchar '.') .>>. term 
+     |>> Abstraction)
+    <|> !var
+    <|> (!(pchar '(') >>. !term .>> pchar ')'
+         |>> Brackets)
+
+let appChain = 
+        !term' .>>. many !term'
+        |>> fun (first, rest) ->
+            List.fold (fun acc t -> Application(acc, Atom t)) (Atom first) rest
+termRef :=     
+    appChain <|> (term' |>> Atom)
+
+let def = !(pstring "let") >>. !var .>> !(pchar '=') .>>. term .>> pchar '\n'
+
+let defs = 
+    many1 !!def
+    |>> (fun pairs -> 
+        let dict = Dictionary<Term', Term>()
+        let rec fillDict list =
+            match list with
+            | head as (name, value) :: tail -> 
+                if name.ToString() <> "let" then dict.Add(name, value) else failwith "'let' cannot be a variable name"
+                fillDict tail
+            | [] -> ()
+        fillDict pairs
+        dict)
+
+let program = !!defs .>>. !term |>> Program
+
+
+let rec toString term = 
+    match term with
+    | LambdaTerm.Variable(x) -> x.ToString()
+    | LambdaTerm.Abstraction(x, body) -> "\\" + x.ToString() + "." + toString body
+    | LambdaTerm.Application(term1, term2) ->
+        let openBracket2, closeBracket2 = 
+            match term2 with
+            | LambdaTerm.Variable(_) -> "", ""
+            | _ -> "(", ")"
+
+        toString term1 + " " + openBracket2 + toString term2 + closeBracket2
